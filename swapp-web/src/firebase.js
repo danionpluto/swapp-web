7a5b0dc6af11b66a92bcbdb492df3945699aa4a9
danionpluto <dr676881@gmail.com>
2024-10-27 15:35:48 -0400

    Created React App and added Firebase

---
+++ swapp-web/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASRnlmC1ByV3chqhf5pJ9S65egl6GdGUA",
+  authDomain: "arc-trade-app.firebaseapp.com",
+  projectId: "arc-trade-app",
+  storageBucket: "arc-trade-app.appspot.com",
+  messagingSenderId: "25333780568",
+  appId: "1:25333780568:web:d3261b4f2ae9e77678ba2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

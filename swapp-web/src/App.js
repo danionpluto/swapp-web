a784d3712b3a34890ccb96d1318355348cce2828
danionpluto <dr676881@gmail.com>
2024-10-29 13:03:49 -0400

    Added Sign Up Page

--- swapp-web/src/App.js
+++ swapp-web/src/App.js
@@ -5,21 +5,24 @@ import Home from './pages/Home';
 import Chat from './pages/Chat';
 import SearchListing from './pages/SearchListing';
 import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <div>
-    <Link to="/">Home</Link>
-    <Link to="/Chat">Chat</Link>
-    <Link to="/Profile">Profile</Link>
-    <Link to="/SearchListing">Search Listing</Link>
+    <Link to="/">Home </Link>
+    <Link to="/Chat">Chat </Link>
+    <Link to="/Profile">Profile </Link>
+    <Link to="/SignUp">Sign Up </Link>
+    <Link to="/SearchListing">Search Listing </Link>
     </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Chat" element={<Chat />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/SearchListing" element={<SearchListing />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ swapp-web/src/pages/SignUp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../firebase.js";
+import { useNavigate } from 'react-router-dom';
+
+
+function SignUp() {
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      // Redirect to another page or perform other actions after successful sign-up
+      navigate('/Home');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+      <div>
+          <label htmlFor="email">First Name:</label>
+          <input type="firstname" id="firstname" value={firstname} onChange={e => setFirstName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="email">Last Name:</label>
+          <input type="lastname" id="lastname" value={lastname} onChange={e => setLastName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;

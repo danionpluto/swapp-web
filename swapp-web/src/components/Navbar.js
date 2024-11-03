1e63fd302b4d6c17d3d0bd20ddb78f41337ef9ea
danionpluto <dr676881@gmail.com>
2024-11-03 17:21:22 -0500

    Nav Bar: react icons

--- swapp-web/src/components/Navbar.js
+++ swapp-web/src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import './Navbar.css'; // Optional: for custom styling
 import {auth} from "../firebase.js";
 import logo from '../pics/logo.png';
+import { FaHome, FaUser, FaTag, FaComments, FaHeart } from 'react-icons/fa';
 
 const Navbar = ({ user }) => {
     const handleLogout = () => {
@@ -14,32 +15,32 @@ const Navbar = ({ user }) => {
       <img src={logo} alt="Logo" className="logo" />
       <ul>
         <li>
-        <Link to="/">Home </Link>
+        <Link className= "link" to="/"><FaHome size={44} /> </Link>
         </li>
         {user ? (
             <>     
         <li>
-        <Link to="/Chat">Chat </Link>
+        <Link  className= "link" to="/Chat"><FaComments size={44} /> </Link>
         </li>
         <li>
-        <Link to="/Profile">Profile </Link>
+        <Link className= "link" to="/SearchListing"><FaTag size={44} /> </Link>
         </li>
         <li>
-        <Link to="/SearchListing">Search Listing </Link>
+        <Link className= "link" to="/Profile"><FaUser size={44} /></Link>
         </li>
         <li>
-              <button onClick={handleLogout}>Logout</button>
+        <button className="nav-button1" onClick={handleLogout}>Log Out</button>
             </li>
         </>
         
         ):(
-            <>
-            <li>
-        <Link to="/SignUp">Sign Up </Link>
+        <>
+        <li>
+        <Link className="nav-button1" to="/SignUp">Sign Up </Link>
         </li>
 
         <li>
-        <Link to="/LogIn">LogIn </Link>
+        <Link className="nav-button2" to="/LogIn">Log In </Link>
         </li>
             </>
         )}
